7ac508976fa94e9cacf1dfadbba4fb9c62c4f177
ipsummit <107095555+ipsummit@users.noreply.github.com>
2025-07-28 20:26:23 -0400

    Create network_provision.py
    Base

---
+++ network_provision.py
@@ -0,0 +1,2 @@
+# plan of action: 1. Scan default network scope and check alive IPs with mac dddress filter as Telematrix devices.
+# If device has vendor mac - try to login via telnet with default login/password and apply basic provisionning settings, then restart the phone.
